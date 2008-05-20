6e194b8424e49b93bc61947e0275d7d326759eeb
aclark <aclark@12379f12-61e2-0310-a953-c5ab0a7db1c5>
2008-05-20 10:41:55 +0000

    Prepare to move ATExtensions trunk to egg.

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='Products.ATExtensions',
+      version=version,
+      description="This package provides some further fields and widgets for archetypes.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Raphael Ritz',
+      author_email='r.ritz@biologie.hu-berlin.de',
+      url='http://svn.plone.org/svn/archetypes/Products.ATExtensions',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
